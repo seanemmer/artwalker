65220d9a3f9f1e07bcccadf3ff354e2385efd281
Sean Emmer <sean.emmer@gmail.com>
2014-09-16 16:00:13 -0400

    Finish sign in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -28,6 +28,8 @@ class SessionsController < ApplicationController
 	end
 
 	def destroy
+		sign_out
+		redirect_to root_path
 	end
 
 	private
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -19,4 +19,12 @@ module SessionsHelper
     remember_token = User.digest(cookies[:remember_token])
     @current_user ||= User.find_by(remember_token: remember_token)
   end
+
+  def sign_out
+    current_user.update_attribute(:remember_token,
+                                  User.digest(User.new_remember_token))
+    cookies.delete(:remember_token)
+    self.current_user = nil
+  end
 end
+
