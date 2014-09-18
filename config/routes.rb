444965dee81887d088dfda6cee816ff9e3b2d221
Sean Emmer <sean.emmer@gmail.com>
2014-09-18 13:33:26 -0400

    Clean configuration absent controllers/DBs

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,9 @@
 SipperV1::Application.routes.draw do
-  resources :sessions, only: [:new, :create, :destroy]
 
   root 'static_pages#home'
   match '/help',   to: 'static_pages#help',   via: 'get'
   match '/about',   to: 'static_pages#about',   via: 'get'
   
-  match '/signin', to: 'sessions#new', via: 'get'
-  match '/signout', to: 'sessions#destroy', via: 'delete'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
